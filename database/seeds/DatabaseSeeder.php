204403683a26c1f66f07ecc5dd6974e44552b7b0
luluq14 <luluq@beritagar.id>
2020-01-18 02:06:49 +0700

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => "Luluq Miftakhul Huda",
+            'email' => 'luluq.ye@gmail.com',
+            'password' => Hash::make('administrator'),
+            'is_deleted' => 0,
+        ]);
     }
 }
